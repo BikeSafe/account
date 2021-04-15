f9b875ba83084e6c2f1381ced69c67eed2e1e5ee
dgonzalezo <dgonzalezo@unal.edu.co>
2021-04-14 22:59:57 -0500

    user authentications and resolvers

---
+++ src/entity/User.ts
@@ -0,0 +1,32 @@
+import { Field, Int, ObjectType } from "type-graphql";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm";
+
+@ObjectType()
+@Entity("users")
+export class User extends BaseEntity {
+  @Field(() => Int)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column("text")
+  firstName: string;
+
+  @Field()
+  @Column("text")
+  lastName: string;
+
+  @Field(() => Int)
+  @Column()
+  age: number;
+
+  @Field()
+  @Column("text")
+  email: string;
+
+  @Column("text")
+  password: string;
+
+  @Column("int", { default: 0 })
+  tokenVersion: number;
+}
